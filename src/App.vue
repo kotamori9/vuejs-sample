aab932b2594b0612ea8c688ea9dd02e6e044e69f
kotamori9 <kotamori27@gmail.com>
2020-04-23 18:41:22 +0900

    componentsファイル作成

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./components/LikeHeader.vue";
+
+  export default{
+    components: {
+      LikeHeader
+    }
+  };
+
+</script>
