83f4ecba68ee397e37182216b80e29bbe321a0d6
kotamori9 <kotamori27@gmail.com>
2020-04-25 11:35:52 +0900

    ケバブケース

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <h2>{{number}}</h2>
+    <LikeNumber :totalNumber="number"></LikeNumber>
+    <LikeNumber :totalNumber="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,6 +11,11 @@
 import LikeHeader from "./components/LikeHeader.vue";
 
   export default{
+    data(){
+      return {
+        number: 10
+      };
+    },
     components: {
       LikeHeader
     }
