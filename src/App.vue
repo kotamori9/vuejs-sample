9324e68d345fd6a39dc07728e43603f13ae77ae9
kotamori9 <kotamori27@gmail.com>
2020-04-25 17:09:19 +0900

    名前付きslot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{number}}</h2>
-    <LikeNumber :totalNumber="number"></LikeNumber>
-    <LikeNumber :totalNumber="number"></LikeNumber>
+    <LikeHeader>
+      <h2>みなさん</h2>
+      <template v-slot:title>
+        <h2>こんにちは</h2>
+      </template>
+      <h3>初めまして</h3>
+      <p>よろしく</p>
+      <template v-slot:number>
+        <p>{{number}}</p>
+      </template>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <LikeNumber :total-number="number" ></LikeNumber>
   </div>
 </template>
 
@@ -13,11 +22,16 @@ import LikeHeader from "./components/LikeHeader.vue";
   export default{
     data(){
       return {
-        number: 10
+        number: 14
       };
     },
     components: {
       LikeHeader
+    },
+    methods: {
+      incrementNumber(value){
+        this.number = value;
+      }
     }
   };
 
@@ -27,4 +41,7 @@ import LikeHeader from "./components/LikeHeader.vue";
   div {
     border: 1px solid blue;
   }
+  /* h1{
+    color: red;
+  } */
 </style>
--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>トータルのいいね数</h1>
+  <div>
+    <slot name= "title"></slot>
+    <slot></slot>
+    <hr>
+    <p>いいねの数</p>
+    <slot name= "number"></slot>
+  </div>
 </template>
+
+<script>
+  export default{
+    props: ["headerText"]
+  };
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+</style>
