e590cba7e915e3265b2f61001a22355f7bfd1128
kotamori9 <kotamori27@gmail.com>
2020-04-26 18:19:41 +0900

    mixin

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+Vue.filter("upperCase", function(value){
+  return value.toUpperCase();
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
